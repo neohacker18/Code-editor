17a9e9168a6bb5ba0b97aaecb324634d5e86738d
Aryan Sethi <46134586+neohacker18@users.noreply.github.com>
2024-01-26 17:35:35 +0530

    containerizing using docker

--- api/generateFile.js
+++ api/generateFile.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
 const path = require("path");
-const { v4: uuid } = require("uuid"); 
+const { v4: uuid } = require("uuid");
 
 const dirCodes = path.join(__dirname, "codes");
 
-if (fs.existsSync(dirCodes)===undefined) {
-  fs.mkdirSync(dircodes, { recursive: true });
+if (!fs.existsSync(dirCodes)) {
+  fs.mkdirSync(dirCodes, { recursive: true });
 }
 
 const generateFile = async (format, content) => {
@@ -13,9 +13,9 @@ const generateFile = async (format, content) => {
   const filename = `${jobId}.${format}`;
   const filepath = path.join(dirCodes, filename);
   await fs.writeFileSync(filepath, content);
-  return {filepath,message:"Success"};
+  return { filepath, message: "Success" };
 };
 
 module.exports = {
-  generateFile
+  generateFile,
 };
